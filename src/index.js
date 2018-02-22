0df7e33503c1e3055522f590880206500c55ddec
Denis Kozyrev <deniskozyrev55@gmail.com>
2018-02-22 20:09:10 +0300

    love triangles implementation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = getLoveTrianglesCount = function(preferences = []) {
+    function isClosedChain(n = 0, anglesCount = 3) {
+        const chain = [];
+
+        let currentIndex = n;
+
+        while (!chain.includes(preferences[currentIndex] - 1)) {
+            chain.push(preferences[currentIndex] - 1);
+            currentIndex = preferences[currentIndex] - 1;
+        }
+
+        return chain.length === anglesCount && chain[chain.length - 1] === n;
+    }
+
+    let trianglesCount = 0;
+    const anglesCount = 3;
+
+    for (let i = 0; i < preferences.length; i++) {
+        if (isClosedChain(i, anglesCount)) {
+            trianglesCount++;
+        }
+    }
+
+    return trianglesCount / anglesCount;
 };
